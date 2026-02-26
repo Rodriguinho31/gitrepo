19b82b010827fbb30d3e7c1347cb8515721ed69f
Rodriguinho31 <99549947+Rodriguinho31@users.noreply.github.com>
2026-02-26 20:34:00 -0300

    Add files via upload

---
+++ SomareDividir/SomareDividir/Calculadora.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomareDividir
+{
+    public class Calculadora
+    {
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+        public double Dividir(int a, int b)
+        {
+            if (b == 0)
+            {
+                
+                Console.WriteLine("Erro, não divide por zero");
+                return 0;
+            }
+            return (double)a / b;
+        }
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        
+        }
+
+        public int Subtrair(int a, int b)
+        {
+            return a - b;
+        }
+
+    }
+}
---
+++ SomareDividir/SomareDividir/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomareDividir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+
+
+            Console.WriteLine("--- Mini Calculadora ---");
+            Console.WriteLine("Soma (10 + 5): " + calc.Somar(10, 5));
+            Console.WriteLine("Divisão (10 / 4): " + calc.Dividir(10, 4));
+            Console.WriteLine("multiplicação (10 * 7): " + calc.Multiplicar(10, 7));
+            Console.WriteLine("subtrair (40 - 10): " + calc.Subtrair(40, 10));
+
+
+            Console.ReadKey();
+        }
+    }
+}
